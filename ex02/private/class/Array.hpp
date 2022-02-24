d8961d20826bbb0ad376c83bbdde4d497545a4df
jodufour <jodufour@student.42.fr>
2022-02-24 21:53:58 +0000

    Add -Wshadow to default CPPFLAGS [OK] Rework ex02                      [OK]

--- ex02/private/class/Array.hpp
+++ ex02/private/class/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 09:04:47 by jodufour          #+#    #+#             */
-/*   Updated: 2022/02/08 05:01:44 by jodufour         ###   ########.fr       */
+/*   Updated: 2022/02/24 21:51:37 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,93 +15,108 @@
 
 # include <iostream>
 
+# ifndef DEBUG
+#  define DEBUG 0
+# endif
+
 template <typename T>
 class Array
 {
 private:
-	T				*_array;
-	unsigned int	_size;
+	// Attributes
+	T		*_array;
+	uint	_size;
 
-protected:
+	class OutOfRangeException : public std::exception
+	{
+	private:
+		// Member functions
+		virtual char const	*what(void) const throw()
+		{
+			return "index is out of range";
+		}
+	};
 
 public:
 	// Constructors
-	Array<T>(void) :
-		_array(NULL),
-		_size(0)
-	{
-		std::cout
-		<< "Default Array<T> constructor"
-		<< std::endl;
-	}
-	Array<T>(Array<T> const &src) :
-		_array(NULL),
-		_size(src._size)
-	{
-		std::cout
-		<< "Copy Array<T> constructor"
-		<< std::endl;
-		*this = src;
-	}
-	Array<T>(unsigned int const n) :
-		_array(NULL),
+	Array<T>(uint const n = 0) :
+		_array(new T[n]()),
 		_size(n)
 	{
-		std::cout
-		<< "Param Array<T> constructor"
-		<< std::endl;
-		this->_array = new T[n]();
+		if (DEBUG)
+			std::cout
+			<< "Creating Array<T>"
+			<< std::endl;
+	}
+	Array<T>(Array<T> const &src) :
+		_array(new T[src._size]()),
+		_size(src._size)
+	{
+		uint	idx;
+
+		if (DEBUG)
+			std::cout
+			<< "Creating Array<T>"
+			<< std::endl;
+		for (idx = 0; idx < _size; ++idx)
+			_array[idx] = src._array[idx];
 	}
 
 	// Destructors
 	virtual ~Array<T>(void) {
-		std::cout
-		<< "Array<T> destructor"
-		<< std::endl;
+		if (DEBUG)
+			std::cout
+			<< "Destroying Array<T>"
+			<< std::endl;
 		delete [] this->_array;
 	}
 
 	// Accessors
 	unsigned int	size(void) const
 	{
+		if (DEBUG)
+			std::cout
+			<< "Calling Array<T>::size()"
+			<< std::endl;
 		return this->_size;
 	}
 
 	// Operators
 	Array	&operator=(Array<T> const &rhs)
 	{
-		unsigned int	i;
+		uint	idx;
 
-		std::cout
-		<< "Array<T> assignment operator"
-		<< std::endl;
+		if (DEBUG)
+			std::cout
+			<< "Calling Array<T>::operator=()"
+			<< std::endl;
 		if (this != &rhs)
 		{
 			delete [] this->_array;
 			this->_array = new T[rhs._size];
-			for (i = 0 ; i < rhs._size ; ++i)
-				this->_array[i] = rhs._array[i];
+			for (idx = 0 ; idx < rhs._size ; ++idx)
+				this->_array[idx] = rhs._array[idx];
 		}
-		return (*this);
+		return *this;
 	}
 
-	T	&operator[](unsigned int const i)
+	T	&operator[](uint const idx)
 	{
-		if (i >= this->_size)
-			throw std::exception();
-		return this->_array[i];
+		if (idx >= this->_size)
+			throw OutOfRangeException();
+		return this->_array[idx];
 	}
 };
 
 template <typename T>
 std::ostream	&operator<<(std::ostream &o, Array<T> &rhs)
 {
-	unsigned int	i;
+	uint	idx;
 
 	o << "Array:" << std::endl;
-	for (i = 0 ; i < rhs.size() ; ++i)
-		o << "\t" "_array[" << i << "]: " << rhs[i] << std::endl;
-	return (o);
+	for (idx = 0 ; idx < rhs.size() ; ++idx)
+		o << "\t" "_array[" << idx << "]: " << rhs[idx] << std::endl;
+	return o;
 }
 
 #endif
